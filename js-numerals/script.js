ef63165fe4a6b663ca23364425d25a22ca7c34e3
Mate Csornyei <mate@csornyei.com>
2020-10-07 20:16:00 +0200

    Input checking

--- js-numerals/script.js
+++ js-numerals/script.js
@@ -1,1 +1,27 @@
-console.log('SCRIPTS WORKING')
+const inputElemenent = document.querySelector('#number');
+const resultElement = document.querySelector('#result');
+const errorElement = document.querySelector('#error');
+
+function onButtonClicked() {
+    showError('')
+    try {
+        const inputText = inputElemenent.value
+        checkInput(inputText);
+
+        resultElement.innerHTML = inputText;
+    } catch (error) {
+        showError(error)
+    }
+}
+
+function checkInput(input) {
+    const parsed = Number(input);
+    if (isNaN(parsed)) {
+        throw new Error('Your input is not a valid number')
+    }
+    return parsed;
+}
+
+function showError(errorMessage) {
+    errorElement.innerHTML = `${errorMessage}`
+}
